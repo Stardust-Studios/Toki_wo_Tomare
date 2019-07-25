1d542d17c8c084be47bb104c9074bd204bff0d7b
MechaDragonX <sonicraghav@gmail.com>
2019-07-24 18:12:36 -0700

    Blocks can be moved by clicking and dragging

--- Dio/Assets/Scripts/BlockBehavior.cs
+++ Dio/Assets/Scripts/BlockBehavior.cs
@@ -12,19 +12,21 @@ public class BlockBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
-        if (
-            Input.GetMouseButtonDown(0)
-            //Input.mousePosition.y > this.GetComponent<BoxCollider2D>().bounds.min.y && Input.mousePosition.y < this.GetComponent<BoxCollider2D>().bounds.max.y &&
-            //Input.mousePosition.x > this.GetComponent<BoxCollider2D>().bounds.min.x && Input.mousePosition.x < this.GetComponent<BoxCollider2D>().bounds.max.x
-            )
-        {
-            this.GetComponent<Transform>().localScale = new Vector3(2, 2, 0);
-            this.GetComponent<Transform>().position = Input.mousePosition;
-        }
-        else
-        {
-            this.GetComponent<Transform>().localScale = new Vector3(1, 1, 0);
-        }
+        //Debug.Log(Input.mousePosition);
+        //if (
+        //    Input.GetMouseButtonDown(0)
+        //    //Input.mousePosition.y > this.GetComponent<BoxCollider2D>().bounds.min.y && Input.mousePosition.y < this.GetComponent<BoxCollider2D>().bounds.max.y &&
+        //    //Input.mousePosition.x > this.GetComponent<BoxCollider2D>().bounds.min.x && Input.mousePosition.x < this.GetComponent<BoxCollider2D>().bounds.max.x
+        //    )
+        //{
+        //    this.gameObject.SetActive(true);
+        //    //this.GetComponent<Transform>().localScale = new Vector3(2, 2, 0);
+        //    this.GetComponent<Transform>().position = Input.mousePosition;
+        //}
+        //else
+        //{
+        //    this.gameObject.SetActive(true);
+        //    this.GetComponent<Transform>().localScale = new Vector3(1, 1, 0);
+        //}
     }
 }
---
+++ Dio/Assets/Scripts/Drag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Drag : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+}

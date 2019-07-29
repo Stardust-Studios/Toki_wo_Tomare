b2200d95b51d6ee0e849098edc823b947d93b263
MechaDragonX <sonicraghav@gmail.com>
2019-07-29 15:39:00 -0700

    Cleaned up movement code and added test pentamino sprites

--- Dio/Assets/Scripts/Drag.cs
+++ Dio/Assets/Scripts/Drag.cs
@@ -6,19 +6,23 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
-    private Vector3 screenPoint;
+    // private Vector3 screenPoint;
     private Vector3 offset;
 
     void OnMouseDown()
     {
 
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        // offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        offset = gameObject.transform.position -
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
     }
 
     void OnMouseDrag()
     {
-        Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
+        // Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        // Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        // transform.position = curPosition;
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
     }
 }
